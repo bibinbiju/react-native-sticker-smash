02219665a48dd3941f751c9cdc18518cda71bd9b
Bibin Biju <bibinvakkom@gmail.com>
2024-11-20 15:34:32 +0530

    initial commit

---
+++ components/EmojiPicker.tsx
@@ -0,0 +1,47 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { PropsWithChildren } from "react";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
+type Props = PropsWithChildren<{
+  isVisible: boolean;
+  onClose: () => void;
+}>;
+export default function EmojiPicker({ isVisible, children, onClose }: Props) {
+  return (
+    <Modal animationType="slide" transparent visible={isVisible}>
+      <View style={styles.modalContent}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Choose a sticker</Text>
+          <Pressable onPress={onClose}>
+            <MaterialIcons name="close" color="#fff" size={22} />
+          </Pressable>
+        </View>
+        {children}
+      </View>
+    </Modal>
+  );
+}
+const styles = StyleSheet.create({
+  modalContent: {
+    height: "25%",
+    width: "100%",
+    backgroundColor: "#2f292e",
+    borderTopRightRadius: 18,
+    borderTopLeftRadius: 18,
+    position: "absolute",
+    bottom: 0,
+  },
+  titleContainer: {
+    height: "16%",
+    borderStartColor: "#464c55",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
